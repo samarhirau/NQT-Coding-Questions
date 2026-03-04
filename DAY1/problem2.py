67aec51985858c14ebeaebe8419938aa397326c7
samarhirau <samrhirau@gmail.com>
2026-03-04 09:02:54 +0530

    solved

--- DAY1/problem2.py
+++ DAY1/problem2.py
@@ -1,17 +1,15 @@
 # Print Prime numbers in range
 
 def print_prime(n):
-    if n > 2:
-        print(2, end=" ")
-    
-    for i in range(3, n, 2):
+    for i in range(2, n):
         is_prime = True
-        for j in range(3, int(i**0.5) + 1, 2):
+        for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
                 is_prime = False
                 break
         if is_prime:
             print(i, end=" ")
+
 
 n = int(input().strip())
 print_prime(n)
