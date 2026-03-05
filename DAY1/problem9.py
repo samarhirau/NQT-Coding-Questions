a87e3b5ab4599e6643955109df6a97a41b77c7a1
samarhirau <samrhirau@gmail.com>
2026-03-05 10:35:04 +0530

    solved

--- DAY1/problem9.py
+++ DAY1/problem9.py
@@ -4,15 +4,13 @@ def is_perfect(num):
     if num <= 1:
         return False
     
-    divisor_sum = 1
+    total_sum = 0
     
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(1, (num // 2) + 1):
         if num % i == 0:
-            divisor_sum += i
-            if i != num // i:
-                divisor_sum += num // i
-    
-    return divisor_sum == num
+            total_sum += i
+            
+    return total_sum == num
 
 
 num = int(input().strip())
