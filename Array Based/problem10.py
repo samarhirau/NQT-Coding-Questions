8927bd5732155184661bb7d1466da1f21eda23bd
samarhirau <samrhirau@gmail.com>
2026-03-05 21:29:02 +0530

    solved

--- Array Based/problem10.py
+++ Array Based/problem10.py
@@ -1,1 +1,17 @@
 # Reverse array 
+
+def reverse_array(arr):
+    start = 0
+    end = len(arr) - 1
+    
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+        
+    return arr
+
+n = int(input())
+arr = list(map(int, input().split()))
+reversed_arr = reverse_array(arr)
+print(*reversed_arr)
